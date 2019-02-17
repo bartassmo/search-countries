155af550e60e865990c94e53f025b889859a4eee
Bartłomiej Smołucha <bartlomiej.smolucha@gmail.com>
2019-02-17 23:09:28 +0100

    change header section

--- script.js
+++ script.js
@@ -4,18 +4,17 @@ $('#search').on('click', searchCountries);
 
 function searchCountries() {
     var countryName = $('#country-name').val();
-    if(!countryName.length) countryName = 'Poland';
+    if (!countryName.length) countryName = 'Poland';
     $.ajax({
-  		url: url + countryName,
-  		method: 'GET',
-  		success: showCountriesList
-  	});
+        url: url + countryName,
+        method: 'GET',
+        success: showCountriesList
+    });
 }
 
 function showCountriesList(resp) {
     countriesList.empty();
-    resp.forEach(function(item) {
+    resp.forEach(function (item) {
         $('<li>').text(item.name).appendTo(countriesList);
-});
+    });
 }
-
